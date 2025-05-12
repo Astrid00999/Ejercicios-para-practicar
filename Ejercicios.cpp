4fb04ca9b618e1f8e9f165dc9505d100bbed396a
Astrid009 <00091525@uca.edu.sv>
2025-05-12 11:43:20 -0600

    Ejercicio realizado

---
+++ Ejercicios.cpp
@@ -0,0 +1,42 @@
+#include <math.h>
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+  int a, b, c;
+  double subrad, x1, x2;
+
+  cout << "Solucion de la ecuacion cuadratica" << endl;
+  cout << "Digite los valores constantes" << endl;
+  cout << "Digite a" << endl;
+  cin >> a;
+
+  cout << "Digite b" << endl;
+  cin >> b;
+
+
+  cout << "Digite c" << endl;
+  cin >> c;
+  
+  subrad = pow(b, 2) - 4 * a * c;
+
+  if (subrad < 0) {
+    cout << "NO EXISTE SOLUCION EN LOS REALES" << endl;
+
+  }
+
+  else {
+    if (subrad == 0) {
+      cout << "Existe una sola raiz" << endl;
+      x1 = -b / (2 * a);
+      cout << "La raiz unica es " << x1 << endl;
+    } else {
+      x1 = (-b + sqrt(subrad)) / (2 * a);
+      x2 = (-b - sqrt(subrad)) / (2 * a);
+    }
+  }
+
+    return 0;
+  }
